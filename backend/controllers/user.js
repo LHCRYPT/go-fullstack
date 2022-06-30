d18d241495d170dcbfa3978f133494ab446463ce
LHCRYPT <l.hamadouche@laposte.net>
2022-06-30 18:31:15 +0200

    modifs exports.login

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -17,29 +17,23 @@ exports.signup = (req, res, next) => { // fonction pour l'enregistrement de nouv
 };
 
 //méthode permettant de vérifier si un utilisateur qui tente de se connecter dispose d'identifiants valides. Implémentons donc notre fonction login
-exports.login = (req, res, next) => { // fonction pour connecter des utilisateurs existants
-    User.findOne({email: req.body.email }) //promesse
-    .then(user => {
-        if (user === null) {
-            res.status(401).json({ message: 'Paire identifiant/mot de passe incorrecte'});
-        } else { 
-        bcrypt.compare(req.body.password, user.password)
-            .then(valid => {
-                if (!valid) {
-                    res.status(401).json({ message: 'Paire identifiant/mot de passe incorrecte'});
-                } else { 
+exports.login = (req, res, next) => {
+    User.findOne({ email: req.body.email })
+        .then(user => {
+            if (!user) {
+                return res.status(401).json({ message: 'Paire login/mot de passe incorrecte'});
+            }
+            bcrypt.compare(req.body.password, user.password)
+                .then(valid => {
+                    if (!valid) {
+                        return res.status(401).json({ message: 'Paire login/mot de passe incorrecte' });
+                    }
                     res.status(200).json({
                         userId: user._id,
                         token: 'TOKEN'
                     });
-                }
-            })
-            .catch(error => { 
-                res.status(500).json( { error } ); //erreurs d'execution de requête dans la base de données
-    })
-  }
-  })
-    .catch(error => { 
-        res.status(500).json( { error } );
-         })
-};
+                })
+                .catch(error => res.status(500).json({ error }));
+        })
+        .catch(error => res.status(500).json({ error }));
+ };
