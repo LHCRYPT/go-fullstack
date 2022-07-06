873655fce62cbd622352a081e5dda3439215342d
LHCRYPT <l.hamadouche@laposte.net>
2022-07-06 16:24:23 +0200

    lignes code pour callback filename const name

--- backend/middleware/multer-config.js
+++ backend/middleware/multer-config.js
@@ -8,10 +8,10 @@ const MIME_TYPES = {
 
 const storage = multer.diskStorage({ // on crée un objet de configuration pour mulder et on l'enregistre sur le disque
   destination: (req, file, callback) => { // dit dans quel dossier enregistrer les fichiers, il y a 3 arguments : req file callback
-    callback(null, 'images');
+    callback(null, 'images'); //on appelle le call back; null pour dire qu'il n'y a pas eu d'erreur à ce niveau là, dossier image en 2ème argument
   },
-  filename: (req, file, callback) => {
-    const name = file.originalname.split(' ').join('_');
+  filename: (req, file, callback) => { //il va dire au multer quel nom utiliser
+    const name = file.originalname.split(' ').join('_'); //on accède au nom pour le fichier avec originalname, split permet de remplacer les espaces dans le nom du fichier par des undescores _
     const extension = MIME_TYPES[file.mimetype];
     callback(null, name + Date.now() + '.' + extension);
   }
