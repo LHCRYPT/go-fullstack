3315e4b0f8e0d74aea6067fe2921c41a62e45f78
LHCRYPT <l.hamadouche@laposte.net>
2022-07-06 07:40:44 +0200

    installation et importation de multer

---
+++ backend/middleware/multer-config.js
@@ -0,0 +1,20 @@
+const multer = require('multer'); //on importe multer
+
+const MIME_TYPES = { //on crée un objet de configuration pour mulder
+  'image/jpg': 'jpg',
+  'image/jpeg': 'jpg',
+  'image/png': 'png'
+};
+
+const storage = multer.diskStorage({
+  destination: (req, file, callback) => {
+    callback(null, 'images');
+  },
+  filename: (req, file, callback) => {
+    const name = file.originalname.split(' ').join('_');
+    const extension = MIME_TYPES[file.mimetype];
+    callback(null, name + Date.now() + '.' + extension);
+  }
+});
+
+module.exports = multer({storage: storage}).single('image');
