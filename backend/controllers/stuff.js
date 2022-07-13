bfd1fa8cb7a176c0d4175bf03a88b02ecc0b3001
LHCRYPT <l.hamadouche@laposte.net>
2022-07-13 08:37:02 +0200

    modification de notre fonction deleteThing()

--- backend/controllers/stuff.js
+++ backend/controllers/stuff.js
@@ -1,5 +1,5 @@
 const Thing = require('../models/thing'); //on importe le thing
-const fs = require('fs'); //??
+const fs = require('fs'); //nouvelle importation. Il s'agit du package fs de Node, fs (file system-système de fichiers) : donne accès aux fonctions pour modifier/supprimer le système de fichiers
 
 
 exports.createThing = (req, res, next) => { //lié à la route post
@@ -16,22 +16,6 @@ exports.createThing = (req, res, next) => { //lié à la route post
     .then(() => { res.status(201).json({message: 'Objet enregistré !'})})
     .catch(error => { res.status(400).json( { error })})
  };
-
-/*exports.createThing = (req, res, next) => { // Placer la route POST au-dessus interceptera les requêtes POST, en les empêchant d'atteindre le middleware GET
-    delete req.body._id; //on supprime l'id qui était dans le champs du body dans le frontend, il est faux + généré par MongonDB automatiquement
-    const thing = new Thing({
-      ...req.body //raccourci JS (opérateur spread) qui copie tous les éléments de req.body (title...)
-    });
-    thing.save() //pour enregistrer thing/l'objet dans la base de donnée, la méthode save() renvoie une Promise
-      .then(() => res.status(201).json({ message: 'Objet enregistré !'})) //Promise (then)pour envoyer une réponse de réussite avec un code 201 de réussite au frontend, sinon expiration de la requête
-      .catch(error => res.status(400).json({ error })); //pour envoyer une réponse avec l'erreur générée par Mongoose ainsi qu'un code d'erreur 400, raccourci js (error)
-  };*/
-
-  /*exports.modifyThing = (req, res, next) => {
-    Thing.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id }) //pour modif un thing dans la base de donnée, 1er argument dans {} et 2ème argu dans {}
-      .then(() => res.status(200).json({ message: 'Objet modifié !'}))
-      .catch(error => res.status(400).json({ error }));
-  };*/
 
   exports.modifyThing = (req, res, next) => {  //lié à la route put
     const thingObject = req.file ? { //extraction de l'objet, on voit s'il y a un champ file
@@ -55,11 +39,30 @@ exports.createThing = (req, res, next) => { //lié à la route post
         });
  };
 
-  exports.deleteThing = (req, res, next) => {
+ exports.deleteThing = (req, res, next) => {
+    Thing.findOne({ _id: req.params.id}) //Nous utilisons l'ID reçue comme paramètre pour accéder au Thing correspondant dans la base de données
+        .then(thing => {
+            if (thing.userId != req.auth.userId) { //Nous vérifions si l’utilisateur qui a fait la requête de suppression est bien celui qui a créé le Thing
+                res.status(401).json({message: 'Not authorized'});
+            } else {
+                const filename = thing.imageUrl.split('/images/')[1]; //Nous utilisons le fait de savoir que notre URL d'image contient un segment /images/ pour séparer le nom de fichier
+                fs.unlink(`images/${filename}`, () => {
+                    Thing.deleteOne({_id: req.params.id})
+                        .then(() => { res.status(200).json({message: 'Objet supprimé !'})})
+                        .catch(error => res.status(401).json({ error }));
+                });
+            }
+        })
+        .catch( error => {
+            res.status(500).json({ error });
+        });
+ };
+
+  /*exports.deleteThing = (req, res, next) => {
     Thing.deleteOne({ _id: req.params.id })
       .then(() => res.status(200).json({ message: 'Objet supprimé !'}))
       .catch(error => res.status(400).json({ error }));
-  };
+  };*/
 
   exports.getOneThing = (req, res, next) => { //nous utilisons la méthode get() pour répondre uniquement aux demandes GET à cet endpoint, nous utilisons deux-points : en face du segment dynamique de la route pour la rendre accessible en tant que paramètre
     Thing.findOne({ _id: req.params.id }) //nous utilisons ensuite la méthode findOne() dans notre modèle Thing pour trouver le Thing unique ayant le même _id que le paramètre de la requête 
